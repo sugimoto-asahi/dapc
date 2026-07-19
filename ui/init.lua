494f779810a13902f812c10b80f82220af768caf
Sugimoto Asahi <sugimoto.asahi99@gmail.com>
2026-07-19 09:56:02 +0900

    chore: clean up unused highlights

--- ui/init.lua
+++ ui/init.lua
@@ -1,12 +1,5 @@
 local breakpoint = require("dapc.ui.breakpoint")
 
--- highlights setup
-local highlights_ns = vim.api.nvim_create_namespace("DapcHighlights")
-vim.api.nvim_set_hl(highlights_ns, "DapcBreakpointSign", { link = "DiagnosticError" })
-vim.api.nvim_set_hl(highlights_ns, "DapcBreakpointLine", {})
-
---- Register all highlights
-vim.api.nvim_set_hl_ns(highlights_ns)
 vim.keymap.set("n", "<leader>db", breakpoint.toggle_breakpoint, {})
 
 local M = {}
